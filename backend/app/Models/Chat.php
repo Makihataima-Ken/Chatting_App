21aab435a9681bf06d25e40ba8bd9eea5143ba5a
Ken <ahmadsoud63@gmail.com>
2024-08-26 12:14:28 +0300

    fully updated models and manged relations among 'em

--- backend/app/Models/Chat.php
+++ backend/app/Models/Chat.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Chat extends Model
 {
     use HasFactory;
+
+    // Mass Assignable Attributes
+    protected $table="chats";
+    protected $guarded = [
+        'id'
+    ];
+
+    //get 'em participants
+    public function participants(): HasMany
+    {
+        return $this->hasMany(ChatParticipant::class, 'chat_id');
+    }
+
+    //get 'em messages
+    public function messages(): HasMany
+    {
+        return $this->hasMany(ChatMessage::class, 'chat_id');
+    }
+
+    // get last message
+    public function lastmessage(): HasOne
+    {
+        return $this->hasOne(ChatMessage::class, 'chat_id')->latest('updated_at');
+    }
 }
--- backend/app/Models/ChatMessage.php
+++ backend/app/Models/ChatMessage.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ChatMessage extends Model
 {
     use HasFactory;
+
+    // Mass Assignable Attributes
+    protected $table="chat_messages";
+    protected $guarded = [
+        'id'
+    ];
+    protected $touches=['chat'];
+
+    //every chat_message has a user
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    //every chat_message has a chat
+    public function chat():BelongsTo
+    {
+        return $this->belongsTo(Chat::class,'chat_id');
+    }
+
 }
